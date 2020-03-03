8d9dc9570541628954af6e9f56a51e45f645a388
Kacper Warda <kacper.warda@logintegra.com>
2020-03-03 15:46:00 +0100

    firts commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Human me = new Human();
+        me.firstName = "Kacper";
+        me.lastName = "Warda";
+        me.pet = new Animal("mouse");
+        me.pet.name = "Myszojeleń";
+
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+
+    }
+}
